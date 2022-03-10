298ca3142425e4294b9a13b516777e5a3c0e2ee0
Ronya <klmirg6@gmail.com>
2022-03-10 15:52:21 -0600

    trying to fix something

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -8,7 +8,7 @@ const typeDefs = gql`
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook([]): User
+    saveBook(): User
     removeBook(bookId: ID!): User
   }
 
