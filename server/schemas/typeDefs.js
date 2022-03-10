f4a2d5a8581e5bf28309cb4dfe6b92c990ff95cd
Ronya <klmirg6@gmail.com>
2022-03-10 02:50:24 -0600

    getting the types set up in typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,12 +2,37 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type Query {
-    helloWorld: String
+    me: User
   }
 
-  // type Mutation {
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook([]): User
+    removeBook(bookId: ID!): User
+  }
 
-  // }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: Img
+    link: Url
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
 `;
 
 
